fece2828829fe0d125b4826b0ae3a165333e387f
clodamigoles <clo.damigoles@gmail.com>
2025-11-08 13:39:04 +0100

    maj

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -3,7 +3,23 @@ import { Html, Head, Main, NextScript } from "next/document"
 export default function Document() {
     return (
         <Html lang="fr">
-            <Head />
+            <Head>
+                {/* Google tag (gtag.js) */}
+                <script
+                    async
+                    src="https://www.googletagmanager.com/gtag/js?id=AW-17674405589"
+                />
+                <script
+                    dangerouslySetInnerHTML={{
+                        __html: `
+                            window.dataLayer = window.dataLayer || [];
+                            function gtag(){dataLayer.push(arguments);}
+                            gtag('js', new Date());
+                            gtag('config', 'AW-17674405589');
+                        `,
+                    }}
+                />
+            </Head>
             <body className="antialiased">
                 <Main />
                 <NextScript />
