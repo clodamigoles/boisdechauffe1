75d83aac0e079cd76346f2a0049054f704bdbaba
clodamigoles <clo.damigoles@gmail.com>
2025-11-08 13:53:31 +0100

    maj

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,3 +1,5 @@
+import Head from "next/head"
+
 import "@/styles/Tailwind.css"
 import "@/styles/Global.css"
 
@@ -6,6 +8,14 @@ import GT from "@/components/ui/GT"
 export default function App({ Component, pageProps }) {
     return (
         <>
+            <Head>
+                <link rel="icon" href="/favicon.ico" />
+                <link rel="icon" type="image/svg+xml" href="/images/logo.svg" />
+                <link rel="apple-touch-icon" href="/images/logo.svg" />
+                <meta property="og:image" content="/images/logo.svg" />
+                <meta name="twitter:card" content="summary_large_image" />
+                <meta name="twitter:image" content="/images/logo.svg" />
+            </Head>
             <GT />
             <Component {...pageProps} />
         </>
